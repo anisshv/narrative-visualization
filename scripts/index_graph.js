9a922eaa381fa6ae97b504e6559a7393e7b7c07d
Anissh <anissh2411@gmail.com>
2023-07-29 18:59:23 -0400

    modified margins

--- scripts/index_graph.js
+++ scripts/index_graph.js
@@ -105,8 +105,8 @@ d3.csv("data/total_emissions_by_year.csv",
         .attr("cy", y(selectedData.total_emission))
       focusText
         .html(selectedData.total_emission + " kilotons of CO2 in " + selectedData.Year)
-        .attr("x", x(selectedData.Year))
-        .attr("y", y(selectedData.total_emission))
+        .attr("x", -margin.top)
+        .attr("y", -margin.left)
     }
     
     function mouseout() {
--- slides/slide1.md
+++ slides/slide1.md
@@ -26,6 +26,8 @@ navigable: false
 
 <script src="//d3js.org/d3.v4.min.js"></script>
 
+<script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>
+
 
 # Agricultural Carbon Dioxide Emissions 
 
