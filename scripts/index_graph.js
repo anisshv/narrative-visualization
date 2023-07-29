698203a4844d343d8430609eef835de7678c2bfe
Anissh <anissh2411@gmail.com>
2023-07-29 18:16:04 -0400

    asdf

--- scripts/index_graph.js
+++ scripts/index_graph.js
@@ -103,10 +103,10 @@ d3.csv("data/total_emissions_by_year.csv",
       focus
         .attr("cx", x(selectedData.Year))
         .attr("cy", y(selectedData.total_emission))
-      // focusText
-        // .html("Year:" + selectedData.Year + "  -  " + "Emissions:" + selectedData.total_emission)
-        // .attr("Year", x(selectedData.Year))
-        // .attr("Emissions", y(selectedData.total_emission))
+      focusText
+        .html("Year:" + selectedData.Year + "  -  " + "Emissions:" + selectedData.total_emission)
+        .attr("cx", x(selectedData.Year))
+        .attr("cy", y(selectedData.total_emission))
     }
     
     function mouseout() {
