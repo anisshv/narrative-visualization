6a9a4eb845b39348c1e7a13a51d1fc9e48efbfa7
Anissh <anissh2411@gmail.com>
2023-07-30 00:13:34 -0400

    poiu

--- scripts/slide1_graph.js
+++ scripts/slide1_graph.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 30, bottom: 40, left: 180},
+var margin = {top: 20, right: 30, bottom: 40, left: 175},
     width = 720 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
