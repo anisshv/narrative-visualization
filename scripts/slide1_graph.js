3fbb687cbd78cd695a99e2d4630c212834ce6eff
Anissh <anissh2411@gmail.com>
2023-07-29 22:37:47 -0400

    ssdfsaf

--- scripts/slide1_graph.js
+++ scripts/slide1_graph.js
@@ -37,24 +37,24 @@ d3.csv("../data/avg_emissions_by_type.csv",
       .call(d3.axisLeft(y));
 
     // This allows to find the closest X index of the mouse:
-    var bisect = d3.bisector(function(d) { return d.Year; }).left;
+    // var bisect = d3.bisector(function(d) { return d.Year; }).left;
 
     // Create the circle that travels along the curve of chart
-    var focus = svg
-      .append('g')
-      .append('circle')
-        .style("fill", "none")
-        .attr("stroke", "black")
-        .attr('r', 8.5)
-        .style("opacity", 0)
+    // var focus = svg
+    //   .append('g')
+    //   .append('circle')
+    //     .style("fill", "none")
+    //     .attr("stroke", "black")
+    //     .attr('r', 8.5)
+    //     .style("opacity", 0)
     
-    // Create the text that travels along the curve of chart
-    var focusText = svg
-      .append('g')
-      .append('text')
-        .style("opacity", 0)
-        .attr("text-anchor", "left")
-        .attr("alignment-baseline", "middle")
+    // // Create the text that travels along the curve of chart
+    // var focusText = svg
+    //   .append('g')
+    //   .append('text')
+    //     .style("opacity", 0)
+    //     .attr("text-anchor", "left")
+    //     .attr("alignment-baseline", "middle")
     
     // // Add X axis label:
     // svg.append("text")
@@ -84,39 +84,39 @@ d3.csv("../data/avg_emissions_by_type.csv",
       .attr("fill", "#69b3a2")
 
     // Create a rect on top of the svg area: this rectangle recovers mouse position
-    svg
-      .append('rect')
-      .style("fill", "none")
-      .style("pointer-events", "all")
-      .attr('width', width)
-      .attr('height', height)
-      .on('mouseover', mouseover)
-      .on('mousemove', mousemove)
-      .on('mouseout', mouseout);
+    // svg
+    //   .append('rect')
+    //   .style("fill", "none")
+    //   .style("pointer-events", "all")
+    //   .attr('width', width)
+    //   .attr('height', height)
+    //   .on('mouseover', mouseover)
+    //   .on('mousemove', mousemove)
+    //   .on('mouseout', mouseout);
     
-    // What happens when the mouse move -> show the annotations at the right positions.
-    function mouseover() {
-      focus.style("opacity", 1)
-      focusText.style("opacity",1)
-    }
+    // // What happens when the mouse move -> show the annotations at the right positions.
+    // function mouseover() {
+    //   focus.style("opacity", 1)
+    //   focusText.style("opacity",1)
+    // }
 
-    function mousemove() {
-      // recover coordinate we need
-      var x0 = x.invert(d3.mouse(this)[0]);
-      var i = bisect(data, x0, 1);
-      selectedData = data[i]
-      focus
-        .attr("cx", x(selectedData.Emissions))
-        .attr("cy", y(selectedData.Type))
-      focusText
-        .html("Year:" + selectedData.Emissions + "  -  " + "Emissions:" + selectedData.Emissions)
-        .attr("Year", x(selectedData.Emissions)+150)
-        .attr("Emissions", y(selectedData.Emissions)+150)
-      }
+    // function mousemove() {
+    //   // recover coordinate we need
+    //   var x0 = x.invert(d3.mouse(this)[0]);
+    //   var i = bisect(data, x0, 1);
+    //   selectedData = data[i]
+    //   focus
+    //     .attr("cx", x(selectedData.Emissions))
+    //     .attr("cy", y(selectedData.Type))
+    //   focusText
+    //     .html("Year:" + selectedData.Emissions + "  -  " + "Emissions:" + selectedData.Emissions)
+    //     .attr("Year", x(selectedData.Emissions)+150)
+    //     .attr("Emissions", y(selectedData.Emissions)+150)
+    //   }
     
-    function mouseout() {
-      focus.style("opacity", 0)
-      focusText.style("opacity", 0)
-    }
+    // function mouseout() {
+    //   focus.style("opacity", 0)
+    //   focusText.style("opacity", 0)
+    // }
 
 })
