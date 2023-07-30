8b35e836c9451bce55202b1ad7362604a225448a
Anissh <anissh2411@gmail.com>
2023-07-30 08:22:28 -0400

    sdfgk

--- scripts/slide2_graph.js
+++ scripts/slide2_graph.js
@@ -171,22 +171,57 @@ d3.csv("../data/avg_emissions_by_country_long.csv",
       .enter()
       .append("rect")
       .attr("x", x(0) )
-      // .attr("x", function(d) { return x(d.Emissions); })
-      // .attr("width", x.bandwidth())
       .attr("y", function(d) { return y(d.Type); })
       .attr("width", function(d) { return x(d.Emissions); })
       .attr("height", y.bandwidth() )
       .attr("fill", "#69b3a2");
     
-    // Create the text that travels along the curve of chart
-    var focusText = svg
-      .append('g')
-      .append('text')
-        .style("opacity", 1)
-        .attr("text-anchor", "left")
-        .attr("alignment-baseline", "middle")
-        .html("6018")
-        .attr("x", margin.top)
-        .attr("y", margin.left + 150)
+        // A function that update the chart
+    function update(selectedGroup) {
+
+      // Create new data with the selection?
+      var dataFilter = data.filter(function(d){return d.Area==selectedGroup})
+
+      var x = d3.scaleLinear()
+        // .domain(d3.extent(data, function(d) { return d.Emissions; }))
+        .domain([0, 7000])
+        .range([ 0, width ]);
+      svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+          .attr("transform", "translate(-10,0)rotate(-45)")
+          .style("text-anchor", "end");
+
+      // Add Y axis
+      var y = d3.scaleBand()
+        .range([ 0, height ])
+        .domain(data.map(function(d) { return d.Type; }))
+        .padding(.1);
+      svg.append("g")
+        .call(d3.axisLeft(y));
+
+      // Give these new data to update line
+      svg.selectAll(".bar")
+        .data(dataFilter)
+        .enter()
+        .append("rect")
+        .merge(".bar")
+        .transition()
+        .duration(1000)
+          .attr("x", x(0) )
+          .attr("y", function(d) { return y(d.Type); })
+          .attr("width", function(d) { return x(d.Emissions); })
+          .attr("height", y.bandwidth() )
+          .attr("fill", "#69b3a2");
+    }
+
+    // // When the button is changed, run the updateChart function
+    d3.select("#selectButton").on("change", function(d) {
+        // recover the option that has been chosen
+        var selectedOption = d3.select(this).property("value");
+        // run the updateChart function with this selected option
+        update(selectedOption);
+    })
 
 })
